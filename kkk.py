d46c5b20a80c319b54e935e0622971cc4fbe2580
Bosco <sdfgvbn@gmail.com>
2024-02-07 12:37:15 +0200

    dfghjgfcdxxcvgb

---
+++ kkk.py
@@ -0,0 +1,5 @@
+a=input('a')
+b=input('b')
+def plus(a,b):
+    return a+b
+print(a,b)
---
+++ sdfgh.py
@@ -0,0 +1,6 @@
+def r():
+    print('hello from bosco')
+    c=67+45
+    x=input('enter number')
+    return x
+r()
